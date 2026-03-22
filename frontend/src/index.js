8748bc07329f5e004e2e6e603c37f7014069fe7d
Linda-31 <lindaxavierdhas3@gmail.com>
2026-03-22 15:38:28 +0530

    update the frontend url

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './custom.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+axios.defaults.baseURL = 'https://mern-hairsalonapp.onrender.com';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
